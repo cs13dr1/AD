0137ca0fb23e1d539a3bd15df181c4bba1369871
cs13dr1 <guinnes83@googlemail.com>
2015-01-27 21:28:26 +0100

    Binärzerlegung

--- FiBo_Matrix.java
+++ FiBo_Matrix.java
@@ -1,5 +1,6 @@
 package FiBo_Matrix;
 
+import java.awt.image.ConvolveOp;
 import java.util.Arrays;
 
 public class FiBo_Matrix {
@@ -7,7 +8,7 @@ public class FiBo_Matrix {
 	public static void main(String[] args) {
 		
 		FiBo_Matrix fiBo = new FiBo_Matrix();
-		int n = 80;
+		int n = 7;
 		
 		// Matrix A zum späteren potenzieren
 		// A = 	( 1 1 )
@@ -37,43 +38,125 @@ public class FiBo_Matrix {
 		System.out.println("matrixRes: " + Arrays.deepToString(matrixRes));
 		
 		System.out.println("");
+		
+//		//------
+//		// n-2 Loops Potenzieren
+//		for (int i = 1; i < n - 2; i++) {
+//		
+//			
+//		matrixC[0][0] = (matrixTemp[0][0]*matrixA[0][0]) + (matrixTemp[0][1]*matrixA[1][0]);
+//		matrixC[0][1] = (matrixTemp[0][0]*matrixA[0][1]) + (matrixTemp[0][1]*matrixA[1][1]);
+//		matrixC[1][0] = (matrixTemp[1][0]*matrixA[0][0]) + (matrixTemp[1][1]*matrixA[1][0]);
+//		matrixC[1][1] = (matrixTemp[1][0]*matrixA[0][1]) + (matrixTemp[1][1]*matrixA[1][1]);
+//		
+//		// Clone matrixC to matrixTemp .. bacause matrixTemp = matrixC did not work :/
+//		matrixTemp = matrixC.clone();
+//		for (int j = 0; j < matrixC.length; j++) {
+//		    matrixTemp[j] = matrixTemp[j].clone();
+//		}
+//			
+//		//System.out.println("matrixTemp: " + i + " : " + Arrays.deepToString(matrixTemp));
+//		System.out.println("matrixC für A^(" + n + "-2) " +  "["+ i + " Loop] " + " : " + Arrays.deepToString(matrixC));
+//		}
+		//------
+		
+		//------
+		// Binärzerlegung von exp(n-2) um Rechenoperationen zu sparen
+		//
+		long[][] matX = {{1,1},{1,0}};
+		long[][] matT = {{1,1},{1,0}};
+		long[][] matRes = {{0,0},{0,0}};
+		
+		// 1) exp(n-2) -> Binärdarstellung
+		// 2) in Array[] speichern
+		// 3) Binärdarstellung auswerten
+		//		0 -> quadrieren
+		//		1 -> quadrieren + multiplizieren
+		// 4) führende 1 wird gestrichen
+		// 5) Operationen entsprechend durchführen
+		int exp = n - 2;
+		String binary = Integer.toBinaryString(exp);
+		String[] array = binary.split("");
+		System.out.println("Binary: " + binary);
+		System.out.println(Arrays.toString(array));
+		System.out.println("");
+		
+		int j = 0;
+		int count = 0;
+		
+		for (int i = 2; i < array.length; i++) {
+			if (array[i].equals("1")) {
+				// quadrieren + multiplizieren
+				System.out.println("Durchlauf " + i + " (QM)");
+				System.out.println("Arrayinhalt: " + array[i]);
 				
-		// n-2 Loops Potenzieren
-		for (int i = 1; i < n - 2; i++) {
-		
+				//Q
+				matRes[0][0] = (matT[0][0]*matT[0][0]) + (matT[0][1]*matT[1][0]);
+				matRes[0][1] = (matT[0][0]*matT[0][1]) + (matT[0][1]*matT[1][1]);
+				matRes[1][0] = (matT[1][0]*matT[0][0]) + (matT[1][1]*matT[1][0]);
+				matRes[1][1] = (matT[1][0]*matT[0][1]) + (matT[1][1]*matT[1][1]);
+				count = count + 1;
+				
+				// Clone matrix
+				matT = matRes.clone();
+				for (j = 0; j < matRes.length; j++) {
+				    matT[j] = matT[j].clone();
+				}
+				
+				//M
+				matRes[0][0] = (matT[0][0]*matX[0][0]) + (matT[0][1]*matX[1][0]);
+				matRes[0][1] = (matT[0][0]*matX[0][1]) + (matT[0][1]*matX[1][1]);
+				matRes[1][0] = (matT[1][0]*matX[0][0]) + (matT[1][1]*matX[1][0]);
+				matRes[1][1] = (matT[1][0]*matX[0][1]) + (matT[1][1]*matX[1][1]);
+				count= count + 1;
+				
+				// Clone matrix
+				matT = matRes.clone();
+				for (j = 0; j < matRes.length; j++) {
+				    matT[j] = matT[j].clone();
+				}
+				
+				
+				} else if (array[i].equals("0")) {
+				// quadrieren
+					System.out.println("Durchlauf " + i + " (Q)");
+				System.out.println("Arrayinhalt: " + array[i]);
+				
+				//Q
+				matRes[0][0] = (matT[0][0]*matT[0][0]) + (matT[0][1]*matT[1][0]);
+				matRes[0][1] = (matT[0][0]*matT[0][1]) + (matT[0][1]*matT[1][1]);
+				matRes[1][0] = (matT[1][0]*matT[0][0]) + (matT[1][1]*matT[1][0]);
+				matRes[1][1] = (matT[1][0]*matT[0][1]) + (matT[1][1]*matT[1][1]);
+				count= count + 1;
+				
+				// Clone matrix
+				matT = matRes.clone();
+				for (j = 0; j < matRes.length; j++) {
+				    matT[j] = matT[j].clone();
+				}
+				
+			}
+				}
 			
-		matrixC[0][0] = (matrixTemp[0][0]*matrixA[0][0]) + (matrixTemp[0][1]*matrixA[1][0]);
-		matrixC[0][1] = (matrixTemp[0][0]*matrixA[0][1]) + (matrixTemp[0][1]*matrixA[1][1]);
-		matrixC[1][0] = (matrixTemp[1][0]*matrixA[0][0]) + (matrixTemp[1][1]*matrixA[1][0]);
-		matrixC[1][1] = (matrixTemp[1][0]*matrixA[0][1]) + (matrixTemp[1][1]*matrixA[1][1]);
 		
-		// Clone matrixC to matrixTemp .. bacause matrixTemp = matrixC did not work :/
-		matrixTemp = matrixC.clone();
-		for (int j = 0; j < matrixC.length; j++) {
-		    matrixTemp[j] = matrixTemp[j].clone();
-		}
-			
-		//System.out.println("matrixTemp: " + i + " : " + Arrays.deepToString(matrixTemp));
-		System.out.println("matrixC für A^(" + n + "-2) " +  "["+ i + " Loop] " + " : " + Arrays.deepToString(matrixC));
-		}
-		
+				
+		System.out.println(Arrays.deepToString(matRes));
+		System.out.println("Anzahl der Operationen: " + count);
 		
 		// Potenzierte matrixC mit matrixD multiplizieren
 		System.out.println("");
-		System.out.println("matrixC: " + Arrays.deepToString(matrixC));
+		System.out.println("matrixC: " + Arrays.deepToString(matRes));
 		System.out.println("matrixD: " + Arrays.deepToString(matrixD));
-		System.out.println("matrixRes = matrixC * matrixD");
-		matrixRes[0][0] = ((matrixC[0][0]*matrixD[0][0]) + (matrixC[0][1]*matrixD[1][0]));
-		matrixRes[1][0] = ((matrixC[1][0]*matrixD[0][0]) + (matrixC[1][1]*matrixD[1][0]));
+		System.out.println("matrix = matRes * matrixD");
+		matrixRes[0][0] = ((matRes[0][0]*matrixD[0][0]) + (matRes[0][1]*matrixD[1][0]));
+		matrixRes[1][0] = ((matRes[1][0]*matrixD[0][0]) + (matRes[1][1]*matrixD[1][0]));
 		System.out.println("matrixRes: " + Arrays.deepToString(matrixRes));
 		//System.out.println("matrixB (Kontrolle): " + Arrays.deepToString(matrixB));
 		
 
 		
 }
-			
-		
-		
+					
 		
 	public long fib(int n) {
 		if(n <= 2) {
